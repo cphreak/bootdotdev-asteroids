6f607ae16b7a78401fc0f0a3d7913e97b133c545
scl <6153394+cphreak@users.noreply.github.com>
2025-03-28 17:18:17 -0700

    Added pygame.tme.Clock

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     numpass, numfail = pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                # print(f"dt= {dt}")
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        #end of loop
+        dt = (clock.tick(60) / 1000 ) #convert millisecond to seconds
 
 if __name__ == "__main__":
     main()
